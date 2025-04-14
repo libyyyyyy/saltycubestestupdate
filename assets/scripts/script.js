88fb4f1f39895a722f4f3bcb7b8fafc868ba9c87
libyyyyyy <libyfemboy@gmail.com>
2025-04-14 09:47:43 -0400

    automation test

---
+++ assets/scripts/script.js
@@ -0,0 +1,46 @@
+// List of games
+const gameList = [
+    {
+      name: "Baldi's Basics",
+      image: "/assets/images/baldisbasics.png",
+      link: "/assets/games/baldisbasics/index.html"
+    },
+    {
+      name: "Another Game",
+      image: "/assets/images/anothergame.png",
+      link: "/assets/games/anothergame/index.html"
+    },
+    // Add more games here
+  ];
+  
+  // Generate game tiles
+  const container = document.getElementById("gamesContainer");
+  
+  gameList.forEach(game => {
+    const div = document.createElement("div");
+    div.className = "gameholder";
+  
+    const a = document.createElement("a");
+    a.href = game.link;
+  
+    const img = document.createElement("img");
+    img.src = game.image;
+    img.alt = game.name;
+  
+    const span = document.createElement("span");
+    span.className = "gt";
+    span.textContent = game.name;
+  
+    a.appendChild(img);
+    div.appendChild(a);
+    div.appendChild(span);
+    container.appendChild(div);
+  });
+  
+  // Toggle compact mode
+  function toggleCompactMode() {
+    const games = document.querySelectorAll('.gameholder');
+    games.forEach(game => {
+      game.classList.toggle('compact');
+    });
+  }
